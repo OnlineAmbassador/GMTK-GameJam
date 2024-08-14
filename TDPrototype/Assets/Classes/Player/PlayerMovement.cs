56fd3f5cd230fbbca31290b034e4bc20f25c9f2a
The-Odis <154756191+The-Odis@users.noreply.github.com>
2024-08-14 16:53:11 -0500

    Created first person character controller and script

--- TDPrototype/Assets/Classes/Player/PlayerMovement.cs
+++ TDPrototype/Assets/Classes/Player/PlayerMovement.cs
@@ -3,19 +3,62 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] Transform PlayerCamera;
-    [SerializeField] private CharacterController Controller;
+    private float xRot;
+    private Vector2 playerMouseInput;
+    private Vector3 playerMovementInput;
+    private Vector3 velocity;
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private CharacterController controller;
     [Space]
-    []
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float sensitivity;
+    [SerializeField] private float gravity = -9.86f;
+    
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        playerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        playerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        MovePlayer();
+        MoveCamera();
+    }
+
+    private void MovePlayer()
+    {
+
         
+
+        if(controller.isGrounded)
+        {
+            velocity.y = -1f;
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y = jumpForce;
+            }
+        }
+        else
+        {
+            velocity.y -= gravity * -2f * Time.deltaTime;
+        }
+
+        Vector3 moveVector = transform.TransformDirection(playerMovementInput);
+        controller.Move(moveVector * speed * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void MoveCamera()
+    {
+        xRot -= playerMouseInput.y * sensitivity;
+
+        transform.Rotate(0f, playerMouseInput.x * sensitivity, 0f);
+        playerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
     }
 }
