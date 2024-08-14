d67d10867939ffbe1fea83e318aef695a03bd7fc
The-Odis <154756191+The-Odis@users.noreply.github.com>
2024-08-14 16:04:41 -0500

    The very beginning of a player controller

---
+++ TDPrototype/Assets/Classes/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] Transform PlayerCamera;
+    [SerializeField] private CharacterController Controller;
+    [Space]
+    []
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
